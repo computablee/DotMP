645fb8163ed49fd426f08be5386815ecd9d3bd16
IAmAPersson <iamaperson620@gmail.com>
2021-05-29 00:28:48 -0500

    wrote a small benchmark

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using OpenMP;
+using System.Diagnostics;
 
 namespace Tester
 {
@@ -7,13 +8,35 @@ namespace Tester
     {
         static void Main(string[] args)
         {
-            Parallel.For(0, 100,
+            const int WORKLOAD = 100000000;
+
+            double[] a = new double[WORKLOAD];
+            double[] b = new double[WORKLOAD];
+            double[] c = new double[WORKLOAD];
+            Random r = new Random();
+
+            for (int i = 0; i < WORKLOAD; i++)
+            {
+                a[i] = r.NextDouble();
+                b[i] = r.NextDouble();
+                c[i] = r.NextDouble();
+            }
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            Parallel.For(0, WORKLOAD,
                 schedule: Parallel.Schedule.Static,
                 num_threads: 8,
+                chunk_size: 128,
                 action: i =>
             {
-                Console.WriteLine(i);
+                a[i] = a[i] * b[i] + c[i];
             });
+
+            s.Stop();
+
+            Console.WriteLine("For loop took {0} milliseconds.", s.ElapsedMilliseconds);
         }
     }
 }
