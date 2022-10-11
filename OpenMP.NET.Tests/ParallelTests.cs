e61a26a4d4f38f24efcfa7d4f8bb89a9704c9c19
Maurizio Pozzobon <maurizio@oiritaly.it>
2022-10-11 17:38:23 +0200

    Translated manual test to automated integration test

---
+++ OpenMP.NET.Tests/ParallelTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace OpenMP.NET.Tests
+{
+    public class ParallelTests
+    {
+        [Fact]
+        public void Parallel_performance_should_be_higher()
+        {
+            var elapsedParallel = Workload(true);
+            var elapsedSeries = Workload(false);
+
+            elapsedParallel.Should().BeLessThan(elapsedSeries);
+        }
+
+        private static long Workload(bool inParallel)
+        {
+            const int WORKLOAD = 1_000_000;
+            const int FORITERS = 20;
+            float[] a = new float[WORKLOAD];
+            float[] b = new float[WORKLOAD];
+            float[] c = new float[WORKLOAD];
+            Random r = new Random();
+
+            for (int i = 0; i < WORKLOAD; i++)
+            {
+                a[i] = (float)r.NextDouble();
+                b[i] = (float)r.NextDouble();
+                c[i] = (float)r.NextDouble();
+            }
+
+            Console.WriteLine("Starting test.");
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            for (int i = 0; i < FORITERS; i++)
+            {
+                if (inParallel)
+                {
+                    Parallel.For(0, WORKLOAD,
+                        num_threads: 4,
+                        action: j => InnerWorkload(j, a, b, c));
+                }
+                else
+                {
+                    for (int j = 0; j < WORKLOAD; j++)
+                    {
+                        InnerWorkload(j, a, b, c);
+                    }
+                }
+            }
+
+            s.Stop();
+            return s.ElapsedMilliseconds;
+        }
+
+        private static void InnerWorkload(int j, float[] a, float[] b, float[] c)
+        {
+            a[j] = (a[j] * b[j] + c[j]) / c[j];
+            while (a[j] > 1000 || a[j] < -1000)
+                a[j] /= 10;
+            int temp = Convert.ToInt32(a[j]);
+            for (int i = 0; i < temp; i++)
+                a[j] += i;
+        }
+    }
+}
