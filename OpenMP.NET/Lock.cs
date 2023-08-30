7d7b27e4f1a4f147b83ce51b3574764b64dafb37
Lane <iamaperson620@gmail.com>
2023-08-30 08:10:31 -0500

    added XML-style comments to lock and locking classes

--- OpenMP.NET/Lock.cs
+++ OpenMP.NET/Lock.cs
@@ -2,28 +2,51 @@ using System.Threading;
 
 namespace OpenMP
 {
+    /// <summary>
+    /// A lock that can be used in a parallel region.
+    /// </summary>
     public class Lock
     {
         volatile internal int _lock;
 
+        /// <summary>
+        /// Constructs a new lock.
+        /// </summary>
         public Lock()
         {
             _lock = 0;
         }
     }
 
+    /// <summary>
+    /// Static class that contains methods for locking.
+    /// </summary>
     public static class Locking
     {
+        /// <summary>
+        /// Stalls the thread until the lock is set.
+        /// </summary>
+        /// <param name="lck">The lock to wait for.</param>
         public static void Set(Lock lck)
         {
             while (Interlocked.CompareExchange(ref lck._lock, 1, 0) == 1) ;
         }
 
+        /// <summary>
+        /// Unsets the lock.
+        /// </summary>
+        /// <param name="lck">The lock to unset.</param>
         public static void Unset(Lock lck)
         {
             Interlocked.Exchange(ref lck._lock, 0);
         }
 
+        /// <summary>
+        /// Attempts to set the lock.
+        /// Does not stall the thread.
+        /// </summary>
+        /// <param name="lck">The lock to attempt to set.</param>
+        /// <returns>True if the lock was set, false otherwise.</returns>
         public static bool Test(Lock lck)
         {
             return Interlocked.CompareExchange(ref lck._lock, 1, 0) == 0;
