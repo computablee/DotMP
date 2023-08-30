0b56e00600461091651891fb8bd88592d73da942
Lane <iamaperson620@gmail.com>
2023-08-30 08:21:24 -0500

    added XML-style comments to init, workshare, and thr

--- OpenMP.NET/Init.cs
+++ OpenMP.NET/Init.cs
@@ -5,13 +5,19 @@ using OpenMP;
 
 namespace OpenMP
 {
-
+    /// <summary>
+    /// Encapsulates a Thread object with information about its progress through a parallel for loop.
+    /// </summary>
     internal class Thr
     {
         internal Thread thread;
         volatile internal int curr_iter;
         internal int working_iter;
 
+        /// <summary>
+        /// Creates a Thr object with the specified Thread object.
+        /// </summary>
+        /// <param name="thread">The Thread object to be encapsulated.</param>
         internal Thr(Thread thread)
         {
             this.thread = thread;
@@ -20,6 +26,9 @@ namespace OpenMP
         }
     }
 
+    /// <summary>
+    /// Contains all relevant information about a parallel for loop.
+    /// </summary>
     internal struct WorkShare
     {
         internal Thr[] threads;
@@ -32,6 +41,11 @@ namespace OpenMP
         internal Operations? op;
         internal List<dynamic> reduction_list;
 
+        /// <summary>
+        /// Creates a WorkShare struct.
+        /// </summary>
+        /// <param name="num_threads">The number of threads to be used in the parallel for loop.</param>
+        /// <param name="threads">The Thread objects to be used in the parallel for loop.</param>
         internal WorkShare(uint num_threads, Thread[] threads)
         {
             this.threads = new Thr[num_threads];
@@ -48,6 +62,10 @@ namespace OpenMP
         }
     }
 
+    /// <summary>
+    /// Contains the WorkShare struct.
+    /// Surely there's a better way to do this. What was I thinking?
+    /// </summary>
     internal static class Init
     {
         internal static WorkShare ws;
