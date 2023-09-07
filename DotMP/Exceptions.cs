bdc508d558be2c7fe1cba4e40ac7bf6281fb5acf
Lane <iamaperson620@gmail.com>
2023-09-07 12:33:12 -0500

    added exception for nested For or ForReduction constructs

--- DotMP/Exceptions.cs
+++ DotMP/Exceptions.cs
@@ -5,7 +5,7 @@ namespace DotMP
     /// <summary>
     /// Exception thrown if a parallel-only construct is used outside of a parallel region.
     /// </summary>
-    class NotInParallelRegionException : Exception
+    public class NotInParallelRegionException : Exception
     {
         public NotInParallelRegionException() { }
 
@@ -17,7 +17,7 @@ namespace DotMP
     /// <summary>
     /// Exception thrown if a sections-only construct is used outside of a sections region.
     /// </summary>
-    class NotInSectionsRegionException : Exception
+    public class NotInSectionsRegionException : Exception
     {
         public NotInSectionsRegionException() { }
 
@@ -29,12 +29,24 @@ namespace DotMP
     /// <summary>
     /// Exception thrown if a Parallel.ParallelRegion is created inside of another Parallel.ParallelRegion.
     /// </summary>
-    class CannotPerformNestedParallelismException : Exception
+    public class CannotPerformNestedParallelismException : Exception
     {
         public CannotPerformNestedParallelismException() { }
 
         public CannotPerformNestedParallelismException(string msg) : base(msg) { }
 
         public CannotPerformNestedParallelismException(string msg, Exception ex) : base(msg, ex) { }
+    }
+
+    /// <summary>
+    /// Exception thrown if a Parallel.For or Parallel.ForReduction<T> is created inside of another Parallel.For or Parallel.ForReduction<T>.
+    /// </summary>
+    public class CannotPerformNestedForException : Exception
+    {
+        public CannotPerformNestedForException() { }
+
+        public CannotPerformNestedForException(string msg) : base(msg) { }
+
+        public CannotPerformNestedForException(string msg, Exception ex) : base(msg, ex) { }
     }
 }
