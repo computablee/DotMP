6a2143c7ed3447da909c7f64a4c9c25643a5e8a1
Lane <iamaperson620@gmail.com>
2023-09-09 01:10:12 -0500

    more controlled access to the tasks queue

--- DotMP/Tasking.cs
+++ DotMP/Tasking.cs
@@ -13,20 +13,63 @@ namespace DotMP
         /// Queue of tasks that must execute.
         /// </summary>
         private static Queue<Action> tasks_pv = new Queue<Action>();
-        /// <summary>
-        /// Getter for singleton queue TaskingContainer.tasks_pv.
-        /// </summary>
-        internal Queue<Action> tasks
-        {
-            get
-            {
-                return tasks_pv;
-            }
-        }
 
         /// <summary>
         /// Default constructor.
         /// </summary>
         internal TaskingContainer() { }
+
+        /// <summary>
+        /// Gets the next task from the queue.
+        /// </summary>
+        /// <returns>
+        /// An (Action, bool) tuple.
+        /// The Action is the next task to execute if there are tasks remaining, otherwise null.
+        /// The bool represents whether or not there was a task remaining to return.
+        /// </returns>
+        internal (Action, bool) GetNextTask()
+        {
+            lock (tasks_pv)
+            {
+                if (tasks_pv.Count > 0)
+                    return (tasks_pv.Dequeue(), true);
+                else
+                    return (null, false);
+            }
+        }
+
+        /// <summary>
+        /// Enqueues a task to the queue.
+        /// </summary>
+        /// <param name="action">The task to enqueue.</param>
+        internal void EnqueueTask(Action action)
+        {
+            lock (tasks_pv)
+            {
+                tasks_pv.Enqueue(action);
+            }
+        }
+
+        /// <summary>
+        /// Enqueues a taskloop task to the task queue.
+        /// </summary>
+        /// <param name="start">The start of the current taskloop task, inclusive.</param>
+        /// <param name="end">The end of the current taskloop task, exclusive.</param>
+        /// <param name="action">The action to be executed.</param>
+        internal void EnqueueTaskloopTask(int start, int end, Action<int> action)
+        {
+            Action loop_action = () =>
+            {
+                for (int i = start; i < end; i++)
+                {
+                    action(i);
+                }
+            };
+
+            lock (tasks_pv)
+            {
+                tasks_pv.Enqueue(loop_action);
+            }
+        }
     }
 }
