8ca1e10ad022e6a0931e2d20f4027ba6c0d1f290
Lane <iamaperson620@gmail.com>
2023-10-12 04:52:09 -0500

    fix linting issues

--- patch_readme.py
+++ patch_readme.py
@@ -1,12 +1,14 @@
 lines = []
 
-with open('README.md', 'r') as file:
+with open("README.md", "r") as file:
     lines = file.readlines()
 
-begin_idx = next(i for i, line in enumerate(lines) if '## Contributors' in line)
-end_idx = next(i for i, line in enumerate(lines) if '<!-- ALL-CONTRIBUTORS-LIST:END -->' in line)
+begin_idx = next(i for i, line in enumerate(lines) if "## Contributors" in line)
+end_idx = next(
+    i for i, line in enumerate(lines) if "<!-- ALL-CONTRIBUTORS-LIST:END -->" in line
+)
 
-del lines[begin_idx:end_idx + 1]
+del lines[begin_idx : end_idx + 1]
 
-with open('ProcessedREADME.md', 'w') as file:
+with open("ProcessedREADME.md", "w") as file:
     file.writelines(lines)
