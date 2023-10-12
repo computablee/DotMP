c415a530e6ac9526578c6cdd4ef0dc041505980c
Lane <iamaperson620@gmail.com>
2023-10-12 04:34:12 -0500

    add python file to patch readme

---
+++ patch_readme.py
@@ -0,0 +1,12 @@
+lines = []
+
+with open('README.md', 'r') as file:
+    lines = file.readlines()
+
+begin_idx = next(i for i, line in enumerate(lines) if '## Contributors' in line)
+end_idx = next(i for i, line in enumerate(lines) if '<!-- ALL-CONTRIBUTORS-LIST:END -->' in line)
+
+del lines[begin_idx:end_idx + 1]
+
+with open('ProcessedREADME.md', 'w') as file:
+    file.writelines(lines)
