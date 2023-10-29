e3f782bab75dc90feaaeedf8e2de3c0b8e96506d
computablee <iamaperson620@gmail.com>
2023-10-29 06:17:37 +0000

    Deploying to gh-pages from @ computablee/DotMP@baf117ed5bd4e53a8969d0961b830d9f243fa2c9 🚀

--- search/classes_a.js
+++ search/classes_a.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['schedule_276',['Schedule',['../classDotMP_1_1Schedule.html',1,'DotMP']]],
   ['sectionscontainer_277',['SectionsContainer',['../classDotMP_1_1SectionsContainer.html',1,'DotMP']]],
-  ['serial_278',['Serial',['../classSerial.html',1,'']]],
+  ['serial_278',['Serial',['../classDotMPTests_1_1Serial.html',1,'DotMPTests']]],
   ['shared_279',['Shared',['../classDotMP_1_1Shared.html',1,'DotMP']]],
   ['shared_3c_20u_20_3e_280',['Shared&lt; U &gt;',['../classDotMP_1_1Shared.html',1,'DotMP']]],
   ['sharedenumerable_281',['SharedEnumerable',['../classDotMP_1_1SharedEnumerable.html',1,'DotMP']]],
--- search/variables_b.js
+++ search/variables_b.js
@@ -6,7 +6,7 @@ var searchData=
   ['selector_492',['selector',['../classDotMP_1_1ForAction.html#af1b6b1722b975c5f302523f353357a6b',1,'DotMP::ForAction']]],
   ['shared_493',['shared',['../classDotMP_1_1Shared.html#a1f326a96f1c6fcdf42e91c10fae2c21a',1,'DotMP::Shared']]],
   ['single_5fthread_494',['single_thread',['../classDotMP_1_1Parallel.html#ab4fa9e263e5088f3c981a7d9c9413225',1,'DotMP::Parallel']]],
-  ['start_495',['start',['../classDotMP_1_1StaticScheduler.html#a46ae485f1926d8c976ee5c3b32ec0ac7',1,'DotMP.StaticScheduler.start()'],['../classDotMP_1_1DynamicScheduler.html#a64483f79c0bae4bee87349aee0745c70',1,'DotMP.DynamicScheduler.start()'],['../classDotMP_1_1GuidedScheduler.html#a4d54db48590bc2dcb7aee7760a8d40d0',1,'DotMP.GuidedScheduler.start()'],['../classSerial.html#a89caf9e9430054d49ab509427dcb47c2',1,'Serial.start()']]],
+  ['start_495',['start',['../classDotMP_1_1StaticScheduler.html#a46ae485f1926d8c976ee5c3b32ec0ac7',1,'DotMP.StaticScheduler.start()'],['../classDotMP_1_1DynamicScheduler.html#a64483f79c0bae4bee87349aee0745c70',1,'DotMP.DynamicScheduler.start()'],['../classDotMP_1_1GuidedScheduler.html#a4d54db48590bc2dcb7aee7760a8d40d0',1,'DotMP.GuidedScheduler.start()'],['../classDotMPTests_1_1Serial.html#ad0a3ea7035fcdae6bbe7ae99a3dd08a8',1,'DotMPTests.Serial.start()']]],
   ['start_5fpv_496',['start_pv',['../classDotMP_1_1WorkShare.html#a72dcd0dfd5be8f9e1f0acb421e7cda4a',1,'DotMP::WorkShare']]],
   ['static_5fscheduler_497',['static_scheduler',['../classDotMP_1_1Schedule.html#ae0b827b8680e389bfb5f5f9f274f1b84',1,'DotMP::Schedule']]]
 ];
