3ec9e92f7e5be66dc7f641bde1a1e4cf102430ca
Lane <iamaperson620@gmail.com>
2023-11-06 03:33:09 -0600

    base implementation for workstealing, sans the stealing part

--- DotMP/Schedule.cs
+++ DotMP/Schedule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace DotMP
@@ -53,6 +54,10 @@ namespace DotMP
         /// Internal holder for the RuntimeScheduler object.
         /// </summary>
         private static RuntimeScheduler runtime_scheduler = new RuntimeScheduler();
+        /// <summary>
+        /// Internal holder for the WorkStealingScheduler object.
+        /// </summary>
+        private static WorkStealingScheduler workstealing_scheduler = new WorkStealingScheduler();
 
         /// <summary>
         /// The static scheduling strategy.
@@ -106,6 +111,21 @@ namespace DotMP
         public static Schedule Runtime { get => runtime_scheduler; }
 
         /// <summary>
+        /// The work-stealing scheduling strategy.
+        /// Each thread gets its own local queue of iterations to execute.
+        /// If a thread's queue is empty, it randomly selects another thread's queue as its "victim" and steals half of its remaining iterations.
+        /// The chunk size parameter specifies how many iterations a thread should execute from its local queue at a time.
+        /// 
+        /// Pros:
+        /// - Good approximation of optimal load balancing.
+        /// - No contention over a shared queue.
+        /// 
+        /// Cons:
+        /// - Stealing can be an expensive operation.
+        /// </summary>
+        public static Schedule WorkStealing { get => workstealing_scheduler; }
+
+        /// <summary>
         /// Abstract method for builtin schedulers to override for implementing IScheduler.
         /// </summary>
         /// <param name="start">The start of the loop, inclusive.</param>
@@ -133,23 +153,23 @@ namespace DotMP
         /// <summary>
         /// The chunk size.
         /// </summary>
-        internal uint chunk_size;
+        private uint chunk_size;
         /// <summary>
         /// Number of threads.
         /// </summary>
-        internal uint num_threads;
+        private uint num_threads;
         /// <summary>
         /// Start of the loop, inclusive.
         /// </summary>
-        internal int start;
+        private int start;
         /// <summary>
         /// End of the loop, exclusive.
         /// </summary>
-        internal int end;
+        private int end;
         /// <summary>
         /// Bookkeeping to check which iteration each thread is on.
         /// </summary>
-        internal int[] curr_iters;
+        private int[] curr_iters;
 
         /// <summary>
         /// Override method for LoopInit, is called when first starting a static loop.
@@ -191,19 +211,19 @@ namespace DotMP
         /// <summary>
         /// The chunk size.
         /// </summary>
-        internal uint chunk_size;
+        private uint chunk_size;
         /// <summary>
         /// Number of threads.
         /// </summary>
-        internal uint num_threads;
+        private uint num_threads;
         /// <summary>
         /// Start of the loop, inclusive.
         /// </summary>
-        internal int start;
+        private int start;
         /// <summary>
         /// End of the loop, exclusive.
         /// </summary>
-        internal int end;
+        private int end;
 
         /// <summary>
         /// Override method for LoopInit, is called when first starting a dynamic loop.
@@ -241,23 +261,23 @@ namespace DotMP
         /// <summary>
         /// The chunk size.
         /// </summary>
-        internal uint chunk_size;
+        private uint chunk_size;
         /// <summary>
         /// Number of threads.
         /// </summary>
-        internal uint num_threads;
+        private uint num_threads;
         /// <summary>
         /// Start of the loop, inclusive.
         /// </summary>
-        internal int start;
+        private int start;
         /// <summary>
         /// End of the loop, exclusive.
         /// </summary>
-        internal int end;
+        private int end;
         /// <summary>
         /// Lock for scheduling purposes.
         /// </summary>
-        internal object sched_lock;
+        private object sched_lock;
 
         /// <summary>
         /// Override method for LoopInit, is called when first starting a guided loop.
@@ -326,6 +346,127 @@ namespace DotMP
         public override void LoopNext(int thread_id, out int start, out int end)
         {
             throw new NotImplementedException("The runtime scheduler isn't meant to be called directly.");
+        }
+    }
+
+    internal sealed class WorkStealingScheduler : Schedule
+    {
+        /// <summary>
+        /// Queue struct, ensuring that no two values share a cache line.
+        /// This avoids false sharing issues.
+        /// </summary>
+        [StructLayout(LayoutKind.Explicit, Size = 192)]
+        private struct Queue
+        {
+            /// <summary>
+            /// Start of the queue.
+            /// </summary>
+            [FieldOffset(0)] internal int start;
+            /// <summary>
+            /// End of the queue.
+            /// </summary>
+            [FieldOffset(64)] internal int end;
+            /// <summary>
+            /// Whether or not there is work remaining in the queue.
+            /// </summary>
+            [FieldOffset(128)] internal bool work_remaining;
+        }
+
+        /// <summary>
+        /// Each thread's queue
+        /// </summary>
+        private Queue[] queues;
+        /// <summary>
+        /// Chunk size to use
+        /// </summary>
+        private uint chunk_size;
+        /// <summary>
+        /// Counts the remaining threads with work so threads know when to stop attempting to steal.
+        /// </summary>
+        private volatile uint threads_with_remaining_work;
+
+        /// <summary>
+        /// Override method for LoopInit, is called when first starting a work-stealing loop.
+        /// </summary>
+        /// <param name="start">The start of the loop, inclusive.</param>
+        /// <param name="end">The end of the loop, exclusive.</param>
+        /// <param name="num_threads">The number of threads.</param>
+        /// <param name="chunk_size">The chunk size.</param>
+        public override void LoopInit(int start, int end, uint num_threads, uint chunk_size)
+        {
+            this.queues = new Queue[num_threads];
+            this.chunk_size = chunk_size;
+            this.threads_with_remaining_work = num_threads;
+
+            int ctr = start;
+            int div = (end - start) / (int)num_threads;
+
+            for (int i = 0; i < num_threads - 1; i++)
+            {
+                queues[i].start = ctr;
+                ctr += div;
+                queues[i].end = ctr;
+                queues[i].work_remaining = true;
+            }
+
+            queues[num_threads - 1].start = ctr;
+            queues[num_threads - 1].end = end;
+            queues[num_threads - 1].work_remaining = true;
+        }
+
+        /// <summary>
+        /// Override method for LoopNext, is called to get the bounds of the next chunk to execute.
+        /// </summary>
+        /// <param name="thread_id">The thread ID.</param>
+        /// <param name="start">The start of the chunk, inclusive.</param>
+        /// <param name="end">The end of the chunk, exclusive.</param>
+        public override void LoopNext(int thread_id, out int start, out int end)
+        {
+            do
+            {
+                start = queues[thread_id].start;
+                end = Math.Min(start + (int)chunk_size, queues[thread_id].end);
+
+                if (start >= end)
+                {
+                    StealHandler(thread_id);
+                }
+                else
+                {
+                    queues[thread_id].start += (int)chunk_size;
+                    return;
+                }
+            }
+            while (threads_with_remaining_work > 0);
+        }
+
+        /// <summary>
+        /// Handles whether or not to steal and how to process the results from a steal.
+        /// </summary>
+        /// <param name="thread_id">The thread ID.</param>
+        private void StealHandler(int thread_id)
+        {
+            if (queues[thread_id].work_remaining)
+            {
+                Interlocked.Decrement(ref threads_with_remaining_work);
+                queues[thread_id].work_remaining = false;
+            }
+
+            if (DoSteal(thread_id))
+            {
+                Interlocked.Increment(ref threads_with_remaining_work);
+                queues[thread_id].work_remaining = true;
+            }
+        }
+
+        /// <summary>
+        /// Perform a steal.
+        /// </summary>
+        /// <param name="thread_id">The thread ID.</param>
+        /// <returns>Whether or not the steal was successful.</returns>
+        private bool DoSteal(int thread_id)
+        {
+            return false;
         }
     }
     #endregion
