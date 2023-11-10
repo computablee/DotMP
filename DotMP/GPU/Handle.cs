528cba09f8b895346c2ccd23ab8957f3a1c979ba
Lane <iamaperson620@gmail.com>
2023-11-10 11:46:13 -0600

    added start offset for index calculations

--- DotMP/GPU/Handle.cs
+++ DotMP/GPU/Handle.cs
@@ -9,12 +9,26 @@ namespace DotMP.GPU
     public struct Index
     {
         /// <summary>
+        /// The start of the for loop, for index calculations.
+        /// </summary>
+        private int start;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="start">The start of the parallel for loop.</param>
+        internal Index(int start)
+        {
+            this.start = start;
+        }
+
+        /// <summary>
         /// Gets the index of the loop.
         /// </summary>
         /// <param name="h">Unused.</param>
         public static implicit operator int(Index h)
         {
-            return Grid.GlobalIndex.X;
+            return Grid.GlobalIndex.X + h.start;
         }
     }
 }
