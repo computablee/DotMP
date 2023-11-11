8b40582bd68e2e4a1b2a825de791739c1bab945e
Lane <iamaperson620@gmail.com>
2023-11-11 10:29:13 -0600

    add missing using

--- DotMP/Parallel.cs
+++ DotMP/Parallel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.ComponentModel;
 using System.Threading;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DotMP
 {
