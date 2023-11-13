1f201884a0b2f5d41ccad7d5138e63d203293c4f
Lane <iamaperson620@gmail.com>
2023-11-13 07:53:33 -0600

    add test for forcollapse

--- DotMP-Tests/GPUTests.cs
+++ DotMP-Tests/GPUTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading;
 using DotMP;
+using DotMP.GPU;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
@@ -69,6 +70,49 @@ namespace DotMPTests
             }
 
             Assert.Equal(a, a_old);
+        }
+
+        /// <summary>
+        /// Tests to make sure that DotMP.GPU.Parallel.ForCollapse produces correct results.
+        /// </summary>
+        [Fact]
+        public void Collapse_works()
+        {
+            int[,] iters_hit = new int[1024, 1024];
+
+            using (var buf = new Buffer<int>(iters_hit, DotMP.GPU.Buffer.Behavior.ToFrom))
+            {
+                DotMP.GPU.Parallel.ParallelForCollapse((258, 512), (512, 600), buf, action: (i, j, iters_hit) =>
+                {
+                    iters_hit[i, j]++;
+                });
+            }
+
+            for (int i = 0; i < 1024; i++)
+                for (int j = 0; j < 1024; j++)
+                    if (i >= 258 && i < 512 && j >= 512 && j < 600)
+                        iters_hit[i, j].Should().Be(1);
+                    else
+                        iters_hit[i, j].Should().Be(0);
+
+            /*iters_hit = null;
+
+            int[,,] iters_hit_3 = new int[128, 128, 64];
+
+            DotMP.Parallel.ParallelForCollapse((35, 64), (16, 100), (10, 62), num_threads: 8, chunk_size: 3, schedule: Schedule.Dynamic, action: (i, j, k) =>
+            {
+                DotMP.Atomic.Inc(ref iters_hit_3[i, j, k]);
+            });
+
+            for (int i = 0; i < 128; i++)
+                for (int j = 0; j < 128; j++)
+                    for (int k = 0; k < 64; k++)
+                        if (i >= 35 && i < 64 && j >= 16 && j < 100 && k >= 10 && k < 62)
+                            iters_hit_3[i, j, k].Should().Be(1);
+                        else
+                            iters_hit_3[i, j, k].Should().Be(0);
+
+            iters_hit_3 = null;*/
         }
 
         private void random_init<T>(T[] arr)
